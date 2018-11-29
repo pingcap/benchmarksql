bb3ea25e297343dab28df734dc9f89848ef288fd
Yu Shuaipeng <jackysp@gmail.com>
2018-11-29 15:49:14 +0800

    use the origin stock level sql

--- src/client/jTPCCConnection.java
+++ src/client/jTPCCConnection.java
@@ -192,8 +192,9 @@ public class jTPCCConnection
 	switch (dbType)
 	{
 	    case jTPCCConfig.DB_POSTGRES:
+	    case jTPCCConfig.DB_MYSQL:
 		stmtStockLevelSelectLow = dbConn.prepareStatement(
-		    "SELECT count(*) AS low_stock FROM (" +
+		    "SELECT /*+ TIDB_INLJ(bmsql_stock) */ count(*) AS low_stock FROM (" +
 		    "    SELECT s_w_id, s_i_id, s_quantity " +
 		    "        FROM bmsql_stock " +
 		    "        WHERE s_w_id = ? AND s_quantity < ? AND s_i_id IN (" +
@@ -204,22 +205,6 @@ public class jTPCCConnection
 		    "                 AND ol_o_id >= d_next_o_id - 20 " +
 		    "                 AND ol_o_id < d_next_o_id " +
 		    "                WHERE d_w_id = ? AND d_id = ? " +
-		    "        ) " +
-		    "    ) AS L");
-		break;
-
-	    case jTPCCConfig.DB_MYSQL:
-		stmtStockLevelSelectLow = dbConn.prepareStatement(
-		    "SELECT /*+ TIDB_INLJ(bmsql_stock) */ count(*) AS low_stock FROM (" +
-		    "    SELECT s_w_id, s_i_id, s_quantity " +
-		    "        FROM bmsql_stock " +
-		    "        WHERE s_w_id = ? AND s_quantity < ? AND s_i_id IN (" +
-		    "            SELECT ol_i_id " +
-		    "                FROM (SELECT * from bmsql_district WHERE d_w_id = ? AND d_id = ?) AS X " +
-		    "                JOIN bmsql_order_line ON ol_w_id = d_w_id " +
-		    "                 AND ol_d_id = d_id " +
-		    "                 AND ol_o_id >= d_next_o_id - 20 " +
-		    "                 AND ol_o_id < d_next_o_id " +
 		    "        ) " +
 		    "    ) AS L");
 		break;
