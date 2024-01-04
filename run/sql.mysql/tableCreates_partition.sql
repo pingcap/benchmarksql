815fdb24b53c523c2883278f3c5d038c6df915ce
Jack Yu <jackysp@gmail.com>
2024-01-05 01:18:38 +0800

    run: fix create partition (#38)
    Signed-off-by: Jack Yu <jackysp@gmail.com>

--- run/sql.mysql/tableCreates_partition.sql
+++ run/sql.mysql/tableCreates_partition.sql
@@ -133,5 +133,5 @@ create table bmsql_stock (
   s_dist_09    char(24),
   s_dist_10    char(24),
   constraint pk_stock primary key (s_w_id, s_i_id)
-) partition by hash(ol_w_id) partitions 1024;
+) partition by hash(s_w_id) partitions 1024;
 
